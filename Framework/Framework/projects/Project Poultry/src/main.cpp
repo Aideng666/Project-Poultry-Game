24c1e6092f5af4dfca549914cae832fe5538abe2
Aiden <aiden.gimpel@ontariotechu.net>
2020-10-08 11:40:09 -0400

    Initialized GLAD and GLFW and added a game window

--- Framework/Framework/projects/Project Poultry/src/main.cpp
+++ Framework/Framework/projects/Project Poultry/src/main.cpp
@@ -1,10 +1,75 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <GLM/glm.hpp>
+#include <wtypes.h>
 
 using namespace std;
 
+
+GLFWwindow* window;
+
+void GetDesktopResolution(int& horizontal, int& vertical)
+{
+	RECT desktop;
+	// Get a handle to the desktop window
+	const HWND hDesktop = GetDesktopWindow();
+	// Get the size of screen to the variable desktop
+	GetWindowRect(hDesktop, &desktop);
+	// The top left corner will have coordinates (0,0)
+	// and the bottom right corner will have coordinates
+	// (horizontal, vertical)
+	horizontal = desktop.right;
+	vertical = desktop.bottom;
+}
+
+bool initGLFW() {
+	if (glfwInit() == GLFW_FALSE) {
+		std::cout << "Failed to Initialize GLFW" << std::endl;
+		return false;
+	}
+
+	int horiz, vert;
+	GetDesktopResolution(horiz, vert);
+	//Create a new GLFW window
+	window = glfwCreateWindow(horiz, vert, "Project Poultry Game", nullptr, nullptr);
+	
+	glfwMakeContextCurrent(window);
+
+	return true;
+}
+
+bool initGLAD() {
+	if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress) == 0) {
+		std::cout << "Failed to initialize Glad" << std::endl;
+		return false;
+	}
+}
+
+
 int main()
 {
-	cout << "Hello World" << "\n";
+	//Initialize GLFW
+	if (!initGLFW())
+		return 1;
 
-	cout << "Welcome to Project Poultry" << endl;
+	//Initialize GLAD
+	if (!initGLAD())
+		return 1;
+
+	//Main Loop//
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+
+
+
+
+
+
+		glfwSwapBuffers(window);
+	}
+
+
+
 }
