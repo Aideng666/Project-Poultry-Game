1c985a9ea0c694c49adb5f65517c965e40c1a5a3
Tyler <tyler.wong@ontariotechu.net>
2020-10-08 11:52:26 -0400

    Tested clear color

--- Framework/Framework/projects/Project Poultry/src/main.cpp
+++ Framework/Framework/projects/Project Poultry/src/main.cpp
@@ -61,15 +61,9 @@ int main()
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 
-
-
-
-
-
+		glClearColor(0.1f, 0.6f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
 
 		glfwSwapBuffers(window);
 	}
-
-
-
 }
