ffe54b14e84a24c5197ea7cda3c44507bffc046f
Aiden <aiden.gimpel@ontariotechu.net>
2020-10-21 12:45:17 -0400

    Started Transform

--- Framework/Framework/projects/Project Poultry/src/Transform.h
+++ Framework/Framework/projects/Project Poultry/src/Transform.h
@@ -1,1 +1,30 @@
 #pragma once
+
+#include "GLM/glm.hpp"
+#include <vector>
+
+#define GLM_ENABLE_EXPERIMENTAL
+
+class Transform {
+
+public:
+
+	Transform();
+
+	glm::vec3 GetPos();
+	glm::vec3 GetScale();
+	glm::vec4 GetRotation();
+
+
+
+
+private:
+	glm::vec3 position;
+	glm::vec3 scale;
+	glm::vec4 rotation;
+	glm::mat4 globalPos;
+	Transform* currentParent;
+	std::vector<Transform*> children;
+
+
+};
