1d6494da5527e93a8488e324961a0c7ec9d5ec0d
Tyler <tyler.wong@ontariotechu.net>
2021-04-07 21:19:37 -0400

    Turned down volume

--- Framework/Framework/projects/Project Poultry/src/main.cpp
+++ Framework/Framework/projects/Project Poultry/src/main.cpp
@@ -65,13 +65,14 @@ int main()
 	Application::SetClearColor(glm::vec4(0.08f, 0.17f, 0.31f, 1.0f));
 
 	scenes.push_back(new MainMenu("Main Menu", window));
-	//scenes.push_back(new Level7("Level 7", window));
-	//scenes.push_back(new Level6("Level 6", window));
-	//scenes.push_back(new MainMenuLevel("Main Menu Level", window));
-	/*scenes.push_back(new Level1("Level 1", window));
+	scenes.push_back(new MainMenuLevel("Main Menu Level", window));
+	scenes.push_back(new Level1("Level 1", window));
 	scenes.push_back(new Level2("Level 2", window));
-	scenes.push_back(new Level3("Level 3", window));*/
-	//scenes.push_back(new Level5("Level 5", window));
+	scenes.push_back(new Level3("Level 3", window));
+	scenes.push_back(new Level4("Level 4", window));
+	scenes.push_back(new Level5("Level 5", window));
+	scenes.push_back(new Level6("Level 6", window));
+	scenes.push_back(new Level7("Level 7", window));
 
 	SetActiveScene(0);
 	 
